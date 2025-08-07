2b6c89fd22df7f62298ab7323b416560dc2268fa
hashimotok-ecsv <meoto2408@gmail.com>
2025-08-08 08:45:54 +0900

    bug fix

--- discord_py_role_panel_lib/events/components/normal_select_event.py
+++ discord_py_role_panel_lib/events/components/normal_select_event.py
@@ -11,7 +11,7 @@ class NormalSelectEvent():
     async def call(self, interaction: discord.Interaction):
         try:
             custom_id: str = interaction.data["custom_id"] #interaction.dataからcustom_idを取り出す
-            if custom_id == "mp_role_panel_edit_select":
+            if custom_id == f"{Func.get_custom_id()}edit_select":
                 value = interaction.data["values"][0]
                 if interaction.user.id not in Func.select_role_panel:
                     await interaction.response.edit_message("役職パネルを選択してください。", view=None)
@@ -25,12 +25,16 @@ class NormalSelectEvent():
                 if embed == None:
                     await interaction.response.edit_message("役職パネルが見つかりません。", view=None)
                     return
-                if embed.fields and embed.fields[1].value == value:
-                    await interaction.response.edit_message(content="重複許可はすでに設定されています。", view=None)
-                    return
                 view: discord.ui.View = discord.ui.View()
-                type_text: str = value
-                component: discord.ui.View = message.components[0]
-                await Func.fix_select_role_panel(interaction=interaction, message=message, type_text=type_text)
+                for text in embed.fields[0].value.split("\n"):
+                    if ":<@&" in text:
+                        emoji_text: str = text.split(":<@&")[0]
+                        role_id: int = int(text.split(":<@&")[1].split(">")[0])
+                        role: discord.Role = interaction.guild.get_role(role_id)
+                        if role != None:
+                            button: discord.ui.Button = discord.ui.Button(label=emoji_text, custom_id=f"{Func.get_custom_id()}{emoji_text}_type_{value}", style=discord.ButtonStyle.gray)
+                            view.add_item(button)
+                await message.edit(view=view)
+                await interaction.response.edit_message(content="役職パネルを更新しました。\n「許可」→「禁止」などの変更時に既存のユーザーが取得しているロールについては各自で対応してください。", view=None)
         except Exception:
             traceback.print_exc()
