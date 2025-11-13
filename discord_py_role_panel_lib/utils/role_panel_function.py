ef0666074df6d4d59f363ed9fffbc3ec08aa8ce7
Monster2408 <meoto2408@gmail.com>
2025-11-13 19:06:34 +0900

    update

--- discord_py_role_panel_lib/utils/role_panel_function.py
+++ discord_py_role_panel_lib/utils/role_panel_function.py
@@ -66,7 +66,11 @@ async def send_role_panel_embed(
     await interaction.response.defer()
     embed: discord.Embed = discord.Embed(title=title, description=description, color=0x00bfff)
     if bot and bot.user:
-        embed.set_footer(text=bot.user.name, icon_url=bot.user.avatar.url)
+        icon = bot.user.display_icon
+        if not icon:
+            embed.set_footer(text=bot.user.name)
+        else:
+            embed.set_footer(text=bot.user.name, icon_url=bot.user.display_icon.url)
     view: discord.ui.View = discord.ui.View()
     type_text: str = None
     if single == "許可":
